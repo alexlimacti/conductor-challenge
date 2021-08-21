3f377d06b055c37f1b4e2b291a1b45c32c63e968
Alexsandro <alexsandrolima.dev@gmail.com>
2021-08-21 20:07:04 -0300

    Inicio do projeto

---
+++ src/test/java/com/muxichallenge/MuxiChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muxichallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuxiChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
